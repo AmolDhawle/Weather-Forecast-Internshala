fc00205c11ec56bf3431a547c3e4dbf132aec775
Amol Dhawle <amoldhawle2@gmail.com>
2024-05-21 15:45:59 +0530

    Adding fetchWeatherDataByCity function

--- src/app.js
+++ src/app.js
@@ -1,15 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const apiKey = 'e5103872dfd9472df7a1fd116bcf67b8'; 
+
   document.getElementById('search-btn').addEventListener('click', handleCitySearch);
+  document.getElementById('current-location-btn').addEventListener('click', getCurrentLocationWeather);
 
   function handleCitySearch() {
     const city = document.getElementById('city-input').value.trim();
     if (city) {
-      console.log(city);
+      fetchWeatherDataByCity(city);
     } else {
       alert('Please enter a valid city name');
     }
   }
+
+  function fetchWeatherDataByCity(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Current weather data:', data); // Log the received data
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        alert('Failed to fetch weather data');
+      });
+  }
+  
 })
 
 
